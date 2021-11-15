f54de099d353db2630f528f4968917d5304fa2cc
TheLeftTomb707 <50126797+TheLeftTomb707@users.noreply.github.com>
2021-11-15 15:15:53 -0700

    Add files via upload

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,136 @@
+player = false;
+function btn1func() {
+    if (player == true) {
+        document.getElementById("btn1").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn1").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn1").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn1").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn2func() {
+    if (player == true) {
+        document.getElementById("btn2").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn2").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn2").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn2").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn3func() {
+    if (player == true) {
+        document.getElementById("btn3").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn3").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn3").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn3").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn4func() {
+    if (player == true) {
+        document.getElementById("btn4").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn4").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn4").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn4").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn5func() {
+    if (player == true) {
+        document.getElementById("btn5").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn5").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn5").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn5").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn6func() {
+    if (player == true) {
+        document.getElementById("btn6").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn6").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn6").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn6").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn7func() {
+    if (player == true) {
+        document.getElementById("btn7").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn7").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn7").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn7").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn8func() {
+    if (player == true) {
+        document.getElementById("btn8").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn8").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn8").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn8").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
+function btn9func() {
+    if (player == true) {
+        document.getElementById("btn9").style.background = "url(images/tic-x.png) white center no-repeat";
+        document.getElementById("btn9").style.backgroundSize = "200px";
+        player = false;
+    }
+    else if (player == false) {  
+        document.getElementById("btn9").style.background = "url(images/tic-o.png) white center no-repeat";
+        document.getElementById("btn9").style.backgroundSize = "200px";
+        player = true;
+    }
+    else {
+        alert("no")
+    }
+}
